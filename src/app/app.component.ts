14387fb403e23396e0da1aaf30498bd932784d9e
hi-isnanis <82354@alumnos.utleon.edu.mx>
2025-09-13 13:49:16 -0600

    lista heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', //archivo que se va a abrir al ejecutar el componente
   standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'basesAngular02';
+  //declarar variables que se muestran en html
+  duplicaNumero(numero:number): number{
+    return numero * 2;
+  }
+  pelicula={
+    titulo: 'Batman',
+    anio: 2022
+  }
 }
